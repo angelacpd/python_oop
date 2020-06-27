56d50283a909d1fe40caa5910e83a0cde22bb623
Angela Crepaldi <angelacrepaldi@gmail.com>
2020-06-27 10:17:14 -0300

    Created Abstract base classes.

--- Chapter_2/abstract_start.py
+++ Chapter_2/abstract_start.py
@@ -1,10 +1,14 @@
 # Using Abstract Base Classes to enforce class constraints
 
+# ABC = Abstract Base Class
+from abc import ABC, abstractmethod
 
-class GraphicShape:
+
+class GraphicShape(ABC):
     def __init__(self):
         super().__init__()
 
+    @abstractmethod     # Each subclass has to override this method
     def calculate_area(self):
         pass
 
@@ -13,15 +17,21 @@ class Circle(GraphicShape):
     def __init__(self, radius):
         self.radius = radius
 
+    def calculate_area(self):
+        return 3.14 * (self.radius ** 2)
+
 
 class Square(GraphicShape):
     def __init__(self, side):
         self.side = side
 
+    def calculate_area(self):
+        return self.side ** 2
 
-g = GraphicShape()
+
+# g = GraphicShape()
 
 c = Circle(10)
 print(c.calculate_area())
 s = Square(12)
-print(c.calculate_area())
+print(s.calculate_area())
