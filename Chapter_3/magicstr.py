8734fe93fdca4bbc0e75fa9be714f088a7ca65f2
Angela Crepaldi <angelacrepaldi@gmail.com>
2020-06-27 11:07:48 -0300

    Added magicstr.py

---
+++ Chapter_3/magicstr.py
@@ -0,0 +1,20 @@
+# Using the __str__ and __repr__ magic methods
+
+
+class Book:
+    def __init__(self, title, author, price):
+        super().__init__()
+        self.title = title
+        self.author = author
+        self.price = price
+
+    # Use the __str__ method to return a string -> user
+
+    # Use the __repr__ method to return an obj representation -> dev
+
+
+b1 = Book("War and Peace", "Leo Tolstoy", 39.95)
+b2 = Book("The Catcher in the Rye", "JD Salinger", 29.95)
+
+print(b1)
+print(b2)
