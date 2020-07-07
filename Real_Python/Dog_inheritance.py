68d6a436e7ddc68636229e9fab58f35d87e395b7
Angela Crepaldi <angelacrepaldi@gmail.com>
2020-07-07 16:43:42 -0300

    RealPython tutorial - Inheritance - Conclusion.

--- Real_Python/Dog_inheritance.py
+++ Real_Python/Dog_inheritance.py
@@ -9,12 +9,14 @@ class Dog:
         return f"{self.name} is {self.age} years old."
 
     def speak(self, sound):
-        return f"{self.name} says {sound}."
+        return f"{self.name} barks: {sound}."
 
 
 # Children classes
 class JackRussellTerrier(Dog):
-    pass
+    def speak(self, sound="Arf"):
+        # return f"{self.name} says {sound}."
+        return super().speak(sound)
 
 
 class Dachshund(Dog):
@@ -25,9 +27,29 @@ class Bulldog(Dog):
     pass
 
 
+class GoldenRetriever(Dog):
+    def speak(self, sound="Bark"):
+        return super().speak(sound)
+
+
 miles = JackRussellTerrier("Miles", 4)
 print(miles)
 buddy = Dachshund("Buddy", 9)
 print(buddy)
 jack = Bulldog("Jack", 3)
 print(jack)
+
+# determine which class a given object belongs to
+print(type(miles))
+
+# determine if miles is also an instance of the Dog class
+print(isinstance(miles, Dog))
+print(isinstance(miles, JackRussellTerrier))
+
+print(miles.speak())
+print(miles.speak("Grrr"))
+print(jack.speak("Woof"))
+
+dobby = GoldenRetriever(name="Dobby", age=6)
+print(dobby)
+print(dobby.speak())
