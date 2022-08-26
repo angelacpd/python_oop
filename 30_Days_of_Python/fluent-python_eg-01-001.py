ddb4898d278a7e1ec6cd2941a4737a9a59c44b3c
Angela Crepaldi <angelacrepaldi@gmail.com>
2022-08-25 21:03:40 -0300

    Update fluent-python_eg-01-001.py

--- 30_Days_of_Python/fluent-python_eg-01-001.py
+++ 30_Days_of_Python/fluent-python_eg-01-001.py
@@ -9,6 +9,8 @@ Example 1-1
 """
 
 import collections
+from random import choice
+
 
 Card = collections.namedtuple('Card', ['rank', 'suit'])
 
@@ -29,61 +31,90 @@ class FrenchDeck:
 
 if __name__ == "__main__":
     deck = FrenchDeck()
-    print(f"length: {len(deck)}")
-    for card in range(0, len(deck)):
+    
+    print(f"length: {len(deck)}\n")
+    
+    for card in range(0, 3):
         print(f"position {card}: {deck[card]}")
-
+    
+    for rank, suit in deck:
+        print(f"{rank} of {suit}")
+    
+    print(f"\nlast position: {deck[-1]}")
+    
+    print(f"\nrandom card: {choice(deck)}")
+    
+    print(f"\nslice deck:\nTop three cards: {deck[:3]}\nAces: {deck[12::13]}")
+    
+    print(f"\nin operator: {Card('A', 'diamonds') in deck}")
+    
+    
 # Output
+
 # length: 52
+
 # position 0: Card(rank='2', suit='spades')
 # position 1: Card(rank='3', suit='spades')
 # position 2: Card(rank='4', suit='spades')
-# position 3: Card(rank='5', suit='spades')
-# position 4: Card(rank='6', suit='spades')
-# position 5: Card(rank='7', suit='spades')
-# position 6: Card(rank='8', suit='spades')
-# position 7: Card(rank='9', suit='spades')
-# position 8: Card(rank='10', suit='spades')
-# position 9: Card(rank='J', suit='spades')
-# position 10: Card(rank='Q', suit='spades')
-# position 11: Card(rank='K', suit='spades')
-# position 12: Card(rank='A', suit='spades')
-# position 13: Card(rank='2', suit='diamonds')
-# position 14: Card(rank='3', suit='diamonds')
-# position 15: Card(rank='4', suit='diamonds')
-# position 16: Card(rank='5', suit='diamonds')
-# position 17: Card(rank='6', suit='diamonds')
-# position 18: Card(rank='7', suit='diamonds')
-# position 19: Card(rank='8', suit='diamonds')
-# position 20: Card(rank='9', suit='diamonds')
-# position 21: Card(rank='10', suit='diamonds')
-# position 22: Card(rank='J', suit='diamonds')
-# position 23: Card(rank='Q', suit='diamonds')
-# position 24: Card(rank='K', suit='diamonds')
-# position 25: Card(rank='A', suit='diamonds')
-# position 26: Card(rank='2', suit='clubs')
-# position 27: Card(rank='3', suit='clubs')
-# position 28: Card(rank='4', suit='clubs')
-# position 29: Card(rank='5', suit='clubs')
-# position 30: Card(rank='6', suit='clubs')
-# position 31: Card(rank='7', suit='clubs')
-# position 32: Card(rank='8', suit='clubs')
-# position 33: Card(rank='9', suit='clubs')
-# position 34: Card(rank='10', suit='clubs')
-# position 35: Card(rank='J', suit='clubs')
-# position 36: Card(rank='Q', suit='clubs')
-# position 37: Card(rank='K', suit='clubs')
-# position 38: Card(rank='A', suit='clubs')
-# position 39: Card(rank='2', suit='hearts')
-# position 40: Card(rank='3', suit='hearts')
-# position 41: Card(rank='4', suit='hearts')
-# position 42: Card(rank='5', suit='hearts')
-# position 43: Card(rank='6', suit='hearts')
-# position 44: Card(rank='7', suit='hearts')
-# position 45: Card(rank='8', suit='hearts')
-# position 46: Card(rank='9', suit='hearts')
-# position 47: Card(rank='10', suit='hearts')
-# position 48: Card(rank='J', suit='hearts')
-# position 49: Card(rank='Q', suit='hearts')
-# position 50: Card(rank='K', suit='hearts')
-# position 51: Card(rank='A', suit='hearts')
+# 2 of spades
+# 3 of spades
+# 4 of spades
+# 5 of spades
+# 6 of spades
+# 7 of spades
+# 8 of spades
+# 9 of spades
+# 10 of spades
+# J of spades
+# Q of spades
+# K of spades
+# A of spades
+# 2 of diamonds
+# 3 of diamonds
+# 4 of diamonds
+# 5 of diamonds
+# 6 of diamonds
+# 7 of diamonds
+# 8 of diamonds
+# 9 of diamonds
+# 10 of diamonds
+# J of diamonds
+# Q of diamonds
+# K of diamonds
+# A of diamonds
+# 2 of clubs
+# 3 of clubs
+# 4 of clubs
+# 5 of clubs
+# 6 of clubs
+# 7 of clubs
+# 8 of clubs
+# 9 of clubs
+# 10 of clubs
+# J of clubs
+# Q of clubs
+# K of clubs
+# A of clubs
+# 2 of hearts
+# 3 of hearts
+# 4 of hearts
+# 5 of hearts
+# 6 of hearts
+# 7 of hearts
+# 8 of hearts
+# 9 of hearts
+# 10 of hearts
+# J of hearts
+# Q of hearts
+# K of hearts
+# A of hearts
+
+# last position: Card(rank='A', suit='hearts')
+
+# random card: Card(rank='J', suit='spades')
+
+# slice deck:
+# Top three cards: [Card(rank='2', suit='spades'), Card(rank='3', suit='spades'), Card(rank='4', suit='spades')]
+# Aces: [Card(rank='A', suit='spades'), Card(rank='A', suit='diamonds'), Card(rank='A', suit='clubs'), Card(rank='A', suit='hearts')]
+
+# in operator: True
